f65aa991538fa3cf40854f543fdb7093fb00977d
Vimal Earnest <v_earnest@yahoo.com>
2013-07-04 03:16:10 +0200

    added code to autmatically checkout the master branch in the submodules

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,19 @@
+# run this from $HOME
+
 #!/bin/bash -xe
 
 WDIR=$(dirname $0)
 
 ln -nsf $WDIR/.emacs-live.el ~/.emacs-live.el
 ln -nsf $WDIR/../emacs-live-packs ~/.emacs-live-packs
+
+for i in $WDIR/*
+do
+  if [ -d "$i" ]; then
+    pushd .
+    cd "$i"
+    echo "switching branch to master on " $PWD
+    git checkout master
+    popd
+  fi
+done
